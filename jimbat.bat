a64bab3227d321c5f32b666f302737f72173b317
Chris Choilee <91154952+ChrChr50@users.noreply.github.com>
2022-07-29 00:35:57 -0600

    Update jimbat.bat

--- jimbat.bat
+++ jimbat.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\ProgramData\Anaconda3\python.exe" "C:\Users\chris\OneDrive\Documents\Cornell\Practice\punjim\jimbot.py"
+"C:\ProgramData\Anaconda3\python.exe" "C:\Users\chris\OneDrive\Documents\timeout.py"
 pause
